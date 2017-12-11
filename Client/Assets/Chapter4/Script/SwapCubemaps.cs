4ac31b6dcb51a0da9eabc0cd4b2e3e1d70c60c10
cpiqq <cpp404@gmail.com>
2017-12-11 17:57:24 +0800

    no message

---
+++ Client/Assets/Chapter4/Script/SwapCubemaps.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SwapCubemaps : MonoBehaviour
+{
+    public Cubemap cubea;
+    public Cubemap cubeb;
+
+    public Transform posa;
+    public Transform posb;
+
+    private Material _curMat;
+
+    private Cubemap _curCube;
+
+    void Awake()
+    {
+        _curMat = GetComponent<Renderer>().material;
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        if (posa)
+        {
+            Gizmos.DrawWireSphere(posa.position, 0.5f);
+        }
+        if (posb)
+        {
+            Gizmos.DrawWireSphere(posb.position, 0.5f);
+        }
+    }
+
+    private Cubemap CheckProbeDistance()
+    {
+        float dista = Vector3.Distance(transform.position, posa.position);
+        float distb = Vector3.Distance(transform.position, posb.position);
+        if (dista < distb)
+        {
+            return cubea;
+        }else if (distb < dista)
+        {
+            return cubeb;
+        }
+        return cubea;
+    }
+
+    void Update()
+    {
+        if (_curMat)
+        {
+            _curCube = CheckProbeDistance();
+            _curMat.SetTexture("_CubeMap",_curCube);
+        }
+    }
+}
